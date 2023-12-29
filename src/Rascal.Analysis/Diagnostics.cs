0872fd4c34b432b8e65ebbddc38aa4dd08ef2a9a
thinker227 <thinker0227@gmail.com>
2023-12-29 13:58:54 +0100

    Remove ThrowAnalayzer

--- src/Rascal.Analysis/Diagnostics.cs
+++ src/Rascal.Analysis/Diagnostics.cs
@@ -13,14 +13,4 @@ public static class Diagnostics
         true,
         "Calling Ok directly inside a Then call is equivalent to calling Map. " +
         "Use Map instead for clarity and performance.");
-    
-    public static DiagnosticDescriptor DoNotThrow { get; } = new(
-        "RASCAL0002",
-        "Do not throw inside a result method or property",
-        "Do not throw exceptions inside method or property '{0}' which returns '{1}'",
-        "Correctness",
-        DiagnosticSeverity.Warning,
-        true,
-        "Methods or properties which return results should not throw exceptions unless absolutely necessary. " +
-            "Instead consider returning `Err<T>()` with a description of the error.");
 }
