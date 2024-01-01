c34532a44c14ef197eb326917796ad9b7fbf2fc4
thinker227 <thinker0227@gmail.com>
2024-01-01 17:46:48 +0100

    Add Equals overloads specifying an equality comparer

--- src/Rascal/Result_Equality.cs
+++ src/Rascal/Result_Equality.cs
@@ -18,6 +18,16 @@ public readonly partial struct Result<T> :
         EqualityCore.Equals(this, other, EqualityComparer<T>.Default);
 
     /// <summary>
+    /// Checks whether the result is ok and the ok value is equal to another value
+    /// using a specified equality comparer.
+    /// </summary>
+    /// <param name="other">The value to check for equality with the ok value of the result.</param>
+    /// <param name="valueComparer">The equality comparer to use for comparing values.</param>
+    [Pure]
+    public bool Equals(T? other, IEqualityComparer<T> valueComparer) =>
+        EqualityCore.Equals(this, other, valueComparer);
+
+    /// <summary>
     /// Checks whether the result is equal to another result.
     /// Results are equal if both results are ok and the ok values are equal,
     /// or if both results are errors.
@@ -26,6 +36,17 @@ public readonly partial struct Result<T> :
     [Pure]
     public bool Equals(Result<T> other) =>
         EqualityCore.Equals(this, other, EqualityComparer<T>.Default);
+
+    /// <summary>
+    /// Checks whether the result is equal to another result using a specified equality comparer.
+    /// Results are equal if both results are ok and the ok values are equal,
+    /// or if both results are errors.
+    /// </summary>
+    /// <param name="other">The result to check for equality with the current result.</param>
+    /// <param name="valueComparer">The equality comparer to use for comparing values.</param>
+    [Pure]
+    public bool Equals(Result<T> other, IEqualityComparer<T> valueComparer) =>
+        EqualityCore.Equals(this, other, valueComparer);
 
     /// <inheritdoc/>
     [Pure]
